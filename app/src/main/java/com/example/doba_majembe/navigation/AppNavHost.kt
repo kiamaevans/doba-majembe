1b1eb08d1193ea399c7b682dcebafd9e6574efd6
njukujames <njukujames@outlook.com>
2026-01-06 14:14:00 +0300

    signup UI

--- app/src/main/java/com/example/doba_majembe/navigation/AppNavHost.kt
+++ app/src/main/java/com/example/doba_majembe/navigation/AppNavHost.kt
@@ -1,2 +1,28 @@
 package com.example.doba_majembe.navigation
 
+<<<<<<< Updated upstream
+=======
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.doba_majembe.ui.theme.screens.LoginScreen
+import com.example.doba_majembe.ui.theme.screens.SignUpScreen
+
+
+@Composable
+fun AppNavHost(
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = ROUTE_LOGIN
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable(ROUTE_LOGIN) { LoginScreen(navController) }
+        composable (ROUTE_SIGNUP){ SignUpScreen(navController) }
+    }
+}
+>>>>>>> Stashed changes
---
+++ app/src/main/java/com/example/doba_majembe/navigation/Routes.kt
@@ -0,0 +1,4 @@
+package com.example.doba_majembe.navigation
+
+const val ROUTE_LOGIN = "login"
+const val ROUTE_SIGNUP = "signup"
